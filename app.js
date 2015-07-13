88951c719d3c25ed83e427181d1e3b53e79bd573
Mart2015 <mgpcamina@hotmail.com>
2015-07-13 18:55:48 +0200

    Marco de la aplicacion

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ var routes = require('./routes/index');
 var app = express();
 
 
+var partials = require('express-partials');
 
 // view engine setup
 
@@ -43,6 +44,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+app.use(partials());
 
 app.use('/', routes);
 
