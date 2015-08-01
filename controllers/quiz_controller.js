b9a92dae760a7d2bbe7c7aa9685a61fe9760a094
Mart2015 <mgpcamina@hotmail.com>
2015-08-01 19:19:07 +0200

    Editar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -54,6 +54,28 @@ exports.new = function(req, res){
                  {quiz: quiz, errors: []});
 };
 
+// PUT /quizes/:id
+exports.update = function(req, res){
+  req.quiz.pregunta = req.body.quiz.pregunta;
+  req.quiz.respuesta = req.body.quiz.respuesta;
+
+  req.quiz
+  .validate()
+  .then(
+     function(err){
+        if (err) {
+           res.render('quizes/edit',
+                 { quiz: req.quiz, errors: err.errors});
+        } else {
+           req.quiz  // save: guarda campos pregunta y respuesta en DB
+           .save( {fields: ["pregunta", "respuesta"]})
+           .then( function(){ res.redirect('/quizes');}); 
+        }   // Redirección HTTP a lista de preguntas (URL relativo)
+      }
+    );
+};
+
+
 // POST /quizes/create
 exports.create = function(req, res) {
   var quiz = models.Quiz.build( req.body.quiz );
@@ -72,6 +94,14 @@ exports.create = function(req, res) {
     }
   );
 };
+
+// GET /quizes/:id/edit
+exports.edit = function(req, res){
+  var quiz = req.quiz; //autoload de instancia de quiz
+  res.render('quizes/edit',
+                 { quiz: quiz, errors: []});
+};
+
 
 // GET /author
 exports.credits = function(req, res) {
